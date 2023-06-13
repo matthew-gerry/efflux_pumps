02408e5f304914648890cb985265124eed27f0b3
Matthew Gerry <mgerry04@gmail.com>
2023-06-13 11:23:13 -0400

    added plot of efflux against ratio of binding affinities

--- plot_efflux_5.py
+++ plot_efflux_5.py
@@ -45,6 +45,40 @@ def plot_efflux_vs_KD(param, KD_axis, Kp, KD_ratio, Kp_ratio, V_base, kappa, cDc
     plt.show()
 
 
+def plot_efflux_vs_ratio(param, KD, Kp, KD_ratio_axis, Kp_ratio, V_base, kappa, cDc, cpp_vals):
+    ''' PLOT THE MEAN EFFLUX AS A FUNCTION OF THE RATIOS BETWEEN DRUG BINDING AFFINITIES '''
+
+    mean_efflux = []
+
+    # Evaluate efflux mean at each value of KD and cpp
+    for i in range(len(cpp_vals)):
+        cpp = cpp_vals[i]
+
+        mean_output = np.vectorize(pump.efflux_numerical_5)(param, KD, Kp, KD_ratio_axis*KD, Kp_ratio*Kp, V_base, kappa, cDc, cpp)
+
+        mean_efflux.append(mean_output)
+
+    # Calculate the energy associated with the conformational transition
+    Et_eV = 6.24e18*param.kB*param.T*np.log(Kp_ratio*KD_ratio_axis) # eV
+
+    # Plot mean values and variances side by side
+    cpp_vals_uM = [1e6*x for x in cpp_vals]
+
+    fig, ax1 = plt.subplots()
+    for i in range(len(cpp_vals)):
+        ax1.semilogx(KD_ratio_axis, mean_efflux[i], label="$[p]_{per} = "+str(round(cpp_vals_uM[i],1))+"\:\mu M$", linestyle = ls_list[i])
+    ax1.set_xlabel("$K_{D,out}/K_{D,in}$")
+    ax1.set_ylabel("$J\:(s^{-1})$")
+    ax1.legend()
+    ax1.ticklabel_format(axis='y', style='scientific', scilimits=(0,0), useMathText=True)
+
+    ax2 = fig.add_axes([0.21,0.37,0.2,0.2])
+    ax2.semilogx(KD_ratio_axis, Et_eV, '-k')
+    ax2.text(0.008,0.1,"$E_t\:(eV)$")
+
+    plt.show()
+
+
 #### GLOBAL VARIABLES ####
 
 ls_list = [(0,(1,1)), "dashdot", "dashed", (0,(3,1,1,1,1,1))] # Linestyle list, for plotting
@@ -61,25 +95,28 @@ cDo = 1e-5 # M
 cpc = 1e-7 # M
 
 
-# Variables
+# Variables - all functions
 Kp = 1e-6 # M, proton binding affinity from periplasm
 Kp_ratio = 1 # M, multiply by Kp to get proton binding affinity from cytoplasm
-KD_ratio = 2 # M, multiply by KD to get drug binding affinity from outside
-V_base = -0.15 # V, base voltage (except plot_specificity)
+V_base = -0.15 # V, base voltage
 kappa = -0.028 # V, voltage dependence on pH difference across the inner membrane
-cDc = 1e-6 # M, cytoplasmic drug concentration (except plot_efflux_vs_D)
+cDc = 1e-6 # M, cytoplasmic drug concentration
 
 
-
-# Axes/lists for plot_efflux_vs_KD
-KD_axis = np.logspace(-9, -1.5, 200) # M, drug binding affinity
+# Variables for plot_efflux_vs_KD
+KD_axis = np.logspace(-8.5, -2.5, 200) # M, drug binding affinity
 cpp_vals = np.array([1e-7, 3e-7, 6e-7, 1e-6]) # M, cytoplasmic drug concentration
+KD_ratio = 2 # M, multiply by KD to get drug binding affinity from outside
+
+# Variables for plot_efflux_vs_ratio
+KD = 1e-6 # M, drug binding affinity from inside
+KD_ratio_axis = np.logspace(-2,3,50) # Ratio of outside to inside drug binding affinities
 
 
 #### MAIN CALLS ####
 
 param = Params3(rD, rp, rt, cDo, cpc, vD, vp) # Create instantiation of Params3 object
-Et = param.kB*param.T*np.log(KD_ratio*Kp_ratio) # Energy change associated with conformational transition (derived)
 
-print(Et)
-plot_efflux_vs_KD(param, KD_axis, Kp, KD_ratio, Kp_ratio, V_base, kappa, cDc, cpp_vals)
+
+# plot_efflux_vs_KD(param, KD_axis, Kp, KD_ratio, Kp_ratio, V_base, kappa, cDc, cpp_vals)
+plot_efflux_vs_ratio(param, KD, Kp, KD_ratio_axis, Kp_ratio, V_base, kappa, cDc, cpp_vals)
