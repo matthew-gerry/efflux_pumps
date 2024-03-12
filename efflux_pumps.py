01e1d471654eab4c4322552978879d02b42a444b
Matthew Gerry <mgerry04@gmail.com>
2024-03-12 17:50:24 -0400

    Worked on a function to calculate the efflux in the p-independent model based on the hand-derived expressions--it is not matching the numerics so far though

--- efflux_pumps.py
+++ efflux_pumps.py
@@ -311,3 +311,20 @@ def efflux_numerical_p_ind(param, KD, cDc, kp_const):
     efflux = SS[2]*R[0,2] - SS[0]*R[2,0] # Efflux at steady state
 
     return efflux
+
+def efflux_analytic_p_ind(param, KD, cDc, kp_const):
+    ''' HAND-DERIVED EXPRESSION FOR THE EFFLUX RATE, ANALOGOUS TO THE ONE FOR THE STANDARD MODEL '''
+    
+    # Effective affinity
+    KM_num = KD*(param.rD*param.vD*KD*(kp_const + param.rt*(1 + param.cDo/KD)) + kp_const*(param.rD + param.rt*(1 + 2*param.cDo/KD)))
+    KM_denom = param.rt + 2*kp_const*param.rD*(1 + param.vD*KD)
+    KM = KM_num/KM_denom
+
+    # Reversibility factor
+    rev = 1 - param.cDo/cDc
+
+    # Prefactor
+    pref = kp_const/(1 + 2*kp_const*(1 + param.vD*KD)/(param.rt*param.vD*KD))
+
+    J = pref*rev*cDc/(cDc + KM)
+    return J
